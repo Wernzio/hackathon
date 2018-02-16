7b18f06d2dac79e6eeb71fdea468964c49df41be
Werner <willem.h.vanzyl@chase.com>
2018-02-16 14:45:11 -0500

    Make domain a env variable

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,6 @@
 var FirebaseServer = require('firebase-server');
 
-new FirebaseServer(5000, 'localhost', {
+new FirebaseServer(5000, `${process.env.TEXT_EDITOR_DOMAIN || 'locahost'}`, {
     states: {
         CA: 'California',
         AL: 'Alabama',
@@ -8,7 +8,7 @@ new FirebaseServer(5000, 'localhost', {
     }
 });
 
-var client = new Firebase('ws://localhost:5000');
+var client = new Firebase(`ws://${process.env.TEXT_EDITOR_DOMAIN || 'locahost'}:5000`);
 client.on('value', function(snap) {
     console.log('Got value: ', snap.val());
 });
